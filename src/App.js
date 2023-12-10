e337674d4ebeadfc8d3e92dcb081064968a7ef35
seanchoii <Vertong05@gmail.com>
2023-12-09 19:51:32 -0700

    initial change

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import {GoogleMap,useLoadScript, MarkerF, InfoWindowF} from '@react-google-maps/api';
+import { useMemo, useState } from "react";
+import { restaurants } from './components/restaurants';
 
 function App() {
+  
+  const center = useMemo(() => ({ lat: 51.0447, lng: -114.0719}), []);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+
+  const handleMapClick = () => {
+    setSelectedRestaurant(null);
+  };
+  
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyDuhuOQcN5FDe7hj8cp2lLrshxsAK1oJRw',
+  });
+  if(!isLoaded){
+    return <></>
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "menu">
+        <h1 style={{fontSize: '40px'}}>Calgary's</h1>
+        <h1 style = {{fontSize: '40px'}}>(Sean)sational</h1>
+        <h1 style = {{fontSize: '40px'}}>(Choi)ces 🍴</h1>
+        <p>- Restraunts in Calgary that I visited ✅</p>
+        <p>- Brutally Honest Reviews ✅</p><br></br>
+        <input type = "checkbox" name = "sushi"></input>
+        <label for = "sushi">Sushi🍣  </label>
+        <input type = "checkbox" name = "korean"></input>
+        <label for = "korean">Korean🍙</label>
+        <small className = "name">seannnchoi@gmail.com <br></br> Email me for a restraunt suggestion!</small>
+      </div>
+      <div className = "collapse">
+        <button></button>
+      </div>
+      <div className = "map">   
+      <GoogleMap
+            mapContainerClassName="map-container"
+            center={center}
+            zoom={10.6}
+            onClick={handleMapClick}
+          >
+            {restaurants.map((restaurant, index) => (
+              <MarkerF
+              key={index}
+              position={{lat:restaurant.latitude, lng: restaurant.longitude}}
+              title={restaurant.name}
+              icon={{
+                url:(require('./assets/sushi-24.svg')).default,
+                scaledSize: new window.google.maps.Size(45,45),
+                size: new window.google.maps.Size(45,45),
+              }}
+              onClick={() => {
+                setSelectedRestaurant(restaurant);
+              }}
+            />
+            ))}
+            {selectedRestaurant && (
+          <InfoWindowF
+            position={{ lat: selectedRestaurant.latitude, lng: selectedRestaurant.longitude }}
+            onCloseClick={() => {
+              setSelectedRestaurant(null);
+            }}
+          >
+            <div>
+              <h2>{selectedRestaurant.name}</h2>
+              <p>Address: {selectedRestaurant.address}</p>
+              {/* Add more details for each restaurant */}
+              <p>{selectedRestaurant.review}</p>
+            </div>
+          </InfoWindowF>
+        )}
+
+          </GoogleMap>      
+      </div>
     </div>
   );
 }
---
+++ src/components/restaurants.js
@@ -0,0 +1,42 @@
+export const restaurants = [
+    {
+        name: "Kinjo Sushi & Grill Dalhousie",
+        address: "5005, 415 Dalhousie Dr NW #415, Calgary, AB T3A 5R8",
+        latitude: 51.10503,
+        longitude: -114.16220,
+        review: "I worked here \n Their potato cake is delicious"
+
+    },
+    
+    {
+        name: "Kinjo Sushi & Grill North Hill",
+        address: "1632 14 Ave NW #1668, Calgary, AB T2N 1M7",
+        latitude: 51.06638,
+        longitude: -114.10081,
+        review: "I worked here \n Their potato cake is delicious"
+    },
+
+    {
+        name: "Ari Sushi",
+        address: "1201 9 Ave SE, Calgary, AB T2G 0S9",
+        latitude: 51.04183968891738,  
+        longitude: -114.0364355301443,
+        review: "I worked here \n Their potato cake is delicious"
+    },
+
+    {
+        name: "Fusion Sushi - Ranchlands",
+        address: "1829 Ranchlands Blvd NW #159, Calgary, AB T3G 2A7",
+        latitude: 51.12340437258305,
+        longitude: -114.19834289677064,
+        review: "I worked here \n Their potato cake is delicious"
+    },
+
+    {
+        name: "Sushi & Kitchen",
+        address: "5620 Signal Hill Centre SW, Calgary, AB T3H 3P8",
+        latitude: 51.019344791750214, 
+        longitude: -114.16882113725244,
+        review: "I worked here \n Their potato cake is delicious"
+    }
+]
