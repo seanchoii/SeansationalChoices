e4751449158e64f781fd92c5bbec651bc14775f5
Sean Choi <Vertong05@gmail.com>
2023-12-14 02:39:45 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ function App() {
   
   const center = useMemo(() => ({ lat: 51.0447, lng: -114.0719}), []);
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
-  const [showSushi, setShowSushi] = useState(false); // State to track sushi checkbox
-  const [showRamen, setShowRamen] = useState(false); // State to track ramen checkbox
-
+  const [showSushi, setShowSushi] = useState(true); 
+  const [showRamen, setShowRamen] = useState(true); 
+  const [showVietnamese, setVietnamese] = useState(true);
+  const [showKorean, setKorean] = useState(true);
+  const [showChicken, setChicken] = useState(true);
+  const [showPizza, setPizza] = useState(true);
+  const [showHamburger, setHamburger] = useState(true);
+  const [showSteak, setSteak] = useState(true);
+  const [showChinese, setChinese] = useState(true);
+  const [showDessert, setDessert] = useState(true);
+  const [showItalian, setItalian] = useState(true);
+  const [showOther, setOther] = useState(true);
+  
   const handleMapClick = () => {
     setSelectedRestaurant(null);
   };
@@ -17,19 +27,45 @@ function App() {
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: 'AIzaSyDuhuOQcN5FDe7hj8cp2lLrshxsAK1oJRw',
   });
-  const filteredRestaurants = restaurants.filter(restaurant => {
-    if (!showSushi && restaurant.type === 'sushi') return true;
-    if (!showRamen && restaurant.type === 'ramen') return true;
-    return false;
-  });
 
-  const handleSushiCheckboxChange = (event) => {
-    setShowSushi(!event.target.checked);
+
+  const handleSushiCheckboxChange = () => {
+    setShowSushi(!showSushi);
   };
-
   const handleRamenCheckboxChange = () => {
     setShowRamen(!showRamen);
   };
+  const handleVietnameseCheckboxChange = () => {
+    setVietnamese(!showVietnamese);
+  }; 
+  const handleKoreanCheckboxChange = () => {
+    setKorean(!showKorean);
+  };
+  const handleChickenCheckboxChange = () => {
+    setChicken(!showChicken);
+  };
+  const handlePizzaCheckboxChange = () => {
+    setPizza(!showPizza);
+  };
+  const handleHamburgerCheckboxChange = () => {
+    setHamburger(!showHamburger);
+  };
+  const handleSteakCheckboxChange = () => {
+    setSteak(!showSteak);
+  };
+  const handleChineseCheckboxChange = () => {
+    setChinese(!showChinese);
+  };
+  const handleDessertCheckboxChange = () => {
+    setDessert(!showDessert);
+  };
+  const handleItalianCheckboxChange = () => {
+    setItalian(!showItalian);
+  };
+  const handleOtherCheckboxChange = () => {
+    setOther(!showOther);
+  };
+ 
   if(!isLoaded){
     return <></>
   }
@@ -41,10 +77,36 @@ function App() {
         <h1 style = {{fontSize: '40px'}}>(Choi)ces 🍴</h1>
         <p>- Restraunts in Calgary that I visited ✅</p>
         <p>- Brutally Honest Reviews ✅</p><br></br>
-        <input type = "checkbox" name = "sushi" checked={!showSushi} onChange={handleSushiCheckboxChange}></input>
-        <label for = "sushi">Sushi🍣  </label>
-        <input type = "checkbox" name = "ramen" checked={!showRamen} onChange={handleRamenCheckboxChange}></input>
-        <label for = "ramen">Ramen🥡</label>
+        <input type = "checkbox" name = "sushi" checked={showSushi} onChange={handleSushiCheckboxChange}></input>
+        <label htmlFor = "sushi">Sushi🍣  </label>
+        <input type = "checkbox" name = "ramen" checked={showRamen} onChange={handleRamenCheckboxChange}></input>
+        <label htmlFor = "ramen">Ramen🥡  </label>
+        <input type = "checkbox" name = "vietnamese" checked = {showVietnamese} onChange={handleVietnameseCheckboxChange}></input>
+        <label htmlFor = "vietnamese">Vietnamese🍜</label><br></br><br></br>
+        <input type = "checkbox" name = "korean" checked = {showKorean} onChange={handleKoreanCheckboxChange}></input>
+        <label htmlFor = "korean">Korean🍙  </label>
+        <input type = "checkbox" name = "chicken" checked = {showChicken} onChange={handleChickenCheckboxChange}></input>
+        <label htmlFor = "chicken">Chicken🍗  </label>
+        <input type = "checkbox" name = "pizza" checked = {showPizza} onChange={handlePizzaCheckboxChange}></input>
+        <label htmlFor = "pizza">Pizza🍕</label> <br></br><br></br>
+        <input type = "checkbox" name = "hamburger" checked = {showHamburger} onChange={handleHamburgerCheckboxChange}></input>
+        <label htmlFor = "hamburger">Hamburger🍔</label>
+        <input type = "checkbox" name = "steak" checked = {showSteak} onChange={handleSteakCheckboxChange}></input>
+        <label htmlFor = "steak">Steak🥩</label>
+        <input type = "checkbox" name = "chinese" checked = {showChinese} onChange={handleChineseCheckboxChange}></input>
+        <label htmlFor = "chinese">Chinese🍚</label><br></br><br></br>
+        <input type = "checkbox" name = "italian" checked = {showItalian} onChange={handleItalianCheckboxChange}></input>
+        <label htmlFor = "italian">Italian🍝</label>
+        <input type = "checkbox" name = "dessert" checked = {showDessert} onChange={handleDessertCheckboxChange}></input>
+        <label htmlFor = "dessert">Dessert🍦</label>
+        <input type = "checkbox" name = "other" checked = {showOther} onChange={handleOtherCheckboxChange}></input>
+        <label htmlFor = "other">Other🌮</label>
+        
+        <div className='randomButton'>
+          <p>Not sure where to eat tonight?</p>
+          <button>Pick a random restaurant</button>
+
+        </div>   
         <small className = "name">seannnchoi@gmail.com <br></br> Email me for a restraunt suggestion!</small>
       </div>
    
@@ -55,13 +117,49 @@ function App() {
             zoom={10.6}
             onClick={handleMapClick}
           >
-            {filteredRestaurants.map((restaurant, index) => {
-            let iconUrl = ''; // Default icon URL
-            if (restaurant.type === 'sushi') {
-              iconUrl = require('./assets/sushi.svg').default; // Sushi icon URL
-            } else if (restaurant.type === 'ramen') {
-              iconUrl = require('./assets/ramen.svg').default; // Ramen icon URL
-            }
+            {restaurants.map((restaurant, index) => {
+              const isSushi = showSushi && restaurant.type === 'sushi';
+              const isRamen = showRamen && restaurant.type === 'ramen';
+              const isVietnamese = showVietnamese && restaurant.type === 'vietnamese';
+              const isKorean = showKorean && restaurant.type === 'korean';
+              const isChicken = showChicken && restaurant.type === 'chicken';
+              const isPizza = showPizza && restaurant.type === 'pizza';
+              const isHamburger = showHamburger && restaurant.type === 'hamburger';
+              const isSteak = showSteak && restaurant.type === 'steak';
+              const isChinese = showChinese && restaurant.type === 'chinese';
+              const isItalian= showItalian && restaurant.type === 'italian';
+              const isDessert= showDessert && restaurant.type === 'dessert';
+              const isOther= showOther && restaurant.type === 'other';
+
+
+              if (isSushi || isRamen || isVietnamese || isKorean || isChicken || isPizza || isHamburger
+                || isSteak || isChinese || isItalian || isDessert || isOther) {
+                let iconUrl = '';
+                if (isSushi) {
+                  iconUrl = require('./assets/sushi.svg').default;
+                } else if (isRamen) {
+                  iconUrl = require('./assets/ramen.svg').default;
+                } else if (isVietnamese) {
+                  iconUrl = require('./assets/noodle.svg').default;
+                } else if (isKorean) {
+                  iconUrl = require('./assets/korean.svg').default;
+                } else if (isChicken) {
+                  iconUrl = require('./assets/chicken.svg').default;
+                } else if (isPizza) {
+                  iconUrl = require('./assets/pizza.svg').default;
+                } else if (isHamburger){
+                  iconUrl = require('./assets/hamburger.svg').default;
+                }else if (isSteak){
+                  iconUrl = require('./assets/steak.svg').default;
+                }else if (isChinese){
+                  iconUrl = require('./assets/dimsum.svg').default;
+                }else if (isItalian){
+                  iconUrl = require('./assets/spaghetti.svg').default;
+                }else if (isDessert){
+                  iconUrl = require('./assets/icecream.svg').default;
+                }else if (isOther){
+                  iconUrl = require('./assets/food.svg').default;
+                }
 
             return (
               <MarkerF
@@ -70,14 +168,16 @@ function App() {
                 title={restaurant.name}
                 icon={{
                   url: iconUrl,
-                  scaledSize: new window.google.maps.Size(35, 35),
-                  size: new window.google.maps.Size(35, 35),
+                  scaledSize: new window.google.maps.Size(30, 30),
+                  size: new window.google.maps.Size(30, 30),
                 }}
                 onClick={() => {
                   setSelectedRestaurant(restaurant);
                 }}
               />
             );
+            }
+            return null;
           })}
             {selectedRestaurant && (
           <InfoWindowF
--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -90,7 +90,34 @@ export const restaurants = [
     },
 
     {
-        name: "Tokyo Street Market",
+        name: "Sensei Bar 🍣",
+        type: 'sushi',
+        address: "1520 14 St SW, Calgary, AB T3C 1C9",
+        latitude:  51.038634239418776, 
+        longitude: -114.09439600381978 ,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 9/10"
+    },
+
+    {
+        name: "Lonely Mouth Bar 🍣",
+        type: 'sushi',
+        address: "528 17 Ave SW, Calgary, AB T2S 0B1",
+        latitude:  51.03800715594472, 
+        longitude: -114.0731023903087 ,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐ 10/10"
+    },
+
+    {
+        name: "BLACK PEPPER ASIAN STREET FAIR 🍣",
+        type: 'sushi',
+        address: "1414 8 St SW #110, Calgary, AB T2R 1J6",
+        latitude:  51.03977644659879, 
+        longitude: -114.08143673604098 ,
+        rating: "⭐⭐ 2/10"
+    },
+
+    {
+        name: "Tokyo Street Market 🥡",
         type: 'ramen',
         address: "922 Centre St NE, Calgary, AB T2E 2P7",
         latitude:  51.06053702174044, 
@@ -99,7 +126,7 @@ export const restaurants = [
     },
     
     {
-        name: "Muku Japanese Ramen",
+        name: "Muku Japanese Ramen 🥡",
         type: 'ramen',
         address: "326 14 St NW, Calgary, AB T2N 1Z7",
         latitude:  51.05403619086431,  
@@ -108,7 +135,7 @@ export const restaurants = [
     },
 
     {
-        name: "Menyatai Japanese Noodles",
+        name: "Menyatai Japanese Noodles 🥡",
         type: 'ramen',
         address: "24 12 St NW, Calgary, AB T2N 1Y3",
         latitude:  51.052170189989,   
@@ -117,7 +144,7 @@ export const restaurants = [
     },
 
     {
-        name: "Shiki Menya",
+        name: "Shiki Menya 🥡",
         type: 'ramen',
         address: "824 1 Ave NE, Calgary, AB T2E 0C3",
         latitude:  51.05336738156965,  
@@ -126,7 +153,7 @@ export const restaurants = [
     },
 
     {
-        name: "JINYA Ramen Bar",
+        name: "JINYA Ramen Bar 🥡",
         type: 'ramen',
         address: "1800 4 St SW #180, Calgary, AB T2S 2S5",
         latitude:  51.03689836216317, 
@@ -135,11 +162,211 @@ export const restaurants = [
     },
 
     {
-        name: "Wami Ootoya Japanese Cuisine",
+        name: "Wami Ootoya Japanese Cuisine 🥡",
         type: 'ramen',
         address: "3820 Brentwood Rd NW #130, Calgary, AB T2L 2L5",
         latitude: 51.08654503927365,  
         longitude: -114.12864035775628,  
         rating: "⭐⭐⭐⭐⭐⭐⭐⭐ 8/10"
     },
+
+    {
+        name: "Pho City 🍜",
+        type: 'vietnamese',
+        address: "207 1 St SE unit 2, Calgary, AB T2G 2G3",
+        latitude: 51.050745054917186,  
+        longitude: -114.06058506235375,  
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐ 10/10"
+    },
+
+    {
+        name: "Noodle & Grill Express 🍜",
+        type: 'vietnamese',
+        address: "2500 University Dr NW, Calgary, AB T2N 4V8",
+        latitude:51.07816514001141, 
+        longitude: -114.13149597492182  ,  
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐ 8/10"
+    },
+
+    {
+        name: "Bake Chef Co. 🍜",
+        type: 'vietnamese',
+        address: "2500 University Dr NW, Calgary, AB T2N 4V8",
+        latitude: 51.07831843020113,  
+        longitude: -114.13167124253299,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 9/10"
+    },
+
+    {
+        name: "Pho Anh 🍜",
+        type: 'vietnamese',
+        address: "400 Crowfoot Crescent NW, Calgary, AB T3G 5H6",
+        latitude: 51.12976016928524,   
+        longitude: -114.20037396869178,
+        rating: "⭐⭐⭐⭐⭐⭐⭐ 7/10"
+    },
+
+    {
+        name: "Pho Hoan Pasteur (Royal Oak) 🍜",
+        type: 'vietnamese',
+        address: "8650 112 Ave NW #8108, Calgary, AB T3R 0R5",
+        latitude: 51.151841165667875,    
+        longitude: -114.2116701070913,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐ 8/10"
+    },
+
+    {
+        name: "Pho X.O. 🍜",
+        type: 'vietnamese',
+        address: "6 Crowfoot Cir NW #109, Calgary, AB T3G 2T3",
+        latitude: 51.123398361500904,   
+        longitude: -114.20310691007455 ,
+        rating: "⭐⭐ 2/10"
+    },
+
+    {
+        name: "Van Son Vietnamese Cuisine 🍜",
+        type: 'vietnamese',
+        address: "308 16 Ave NW, Calgary, AB T2M 0H6",
+        latitude: 51.06721282460241,   
+        longitude: -114.0679572435471,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐ 8/10"
+    },
+
+    {
+        name: "Izumi Kitchen & Bar 🍙",
+        type: 'korean',
+        address: "935 7 Ave SW, Calgary, AB T2P 1A5",
+        latitude: 51.04691412052098,  
+        longitude:-114.08327257211921 ,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐ 10/10"
+    },
+
+    {
+        name: "Jjamppong 🍙",
+        type: 'korean',
+        address: "5802 Macleod Trail SW #100, Calgary, AB T2H 0J8",
+        latitude: 51.00238298000308,   
+        longitude:-114.07131895702435,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 9/10"
+    },
+
+    {
+        name: "Jjawang 🍙",
+        type: 'korean',
+        address: "40 Christie Park View SW #1150, Calgary, AB T3H 6E7",
+        latitude: 51.03738447884853, 
+        longitude: -114.17674880246204,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐ 8/10"
+    },
+
+    {
+        name: "Han Corea 🍙",
+        type: 'korean',
+        address: "7008 Farrell Rd SE, Calgary, AB T2H 0T2",
+        latitude: 50.99224673585495, 
+        longitude: -114.05998674192944,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐ 8/10"
+    },
+
+    {
+        name: "Bowls Korean BBQ 🍙",
+        type: 'korean',
+        address: "9650 Harvest Hills Blvd N #1301, Calgary, AB T3K 0B3",
+        latitude:51.14101001764086, 
+        longitude: -114.0700279025381,
+        rating: "⭐⭐⭐⭐ 4/10"
+    },
+
+    {
+        name: "Hanbo Korean Noodle 🍙",
+        type: 'korean',
+        address: "4340 Macleod Trail SW, Calgary, AB T2G 0A4",
+        latitude: 51.01469072197202, 
+        longitude: -114.06452146013504,
+        rating: "⭐⭐⭐⭐⭐ 5/10"
+    },
+
+    {
+        name: "San Dong Banjeom 🍙",
+        type: 'korean',
+        address: "3803 26 Ave SW, Calgary, AB T3E 6V7",
+        latitude: 51.029928418239834, 
+        longitude: -114.14183101646614,
+        rating: "⭐⭐⭐⭐ 4/10"
+    },
+
+    {
+        name: "Manna Korean Cuisine 🍙",
+        type: 'korean',
+        address: "3400 14 St NW #104, Calgary, AB T2K 1H9",
+        latitude: 51.082585527122255, 
+        longitude: -114.09429906506844,
+        rating: "⭐⭐⭐ 3/10"
+    },
+
+    {
+        name: "Seoul Fried Chicken Calgary 🍗",
+        type: 'chicken',
+        address: "2100 4 St SW, Calgary, AB T2S 1W7",
+        latitude: 51.03402392611848,  
+        longitude: -114.07131479167954,
+        rating: "⭐ 1/10"
+    },
+
+    {
+        name: "Chicko Chicken 🍗",
+        type: 'chicken',
+        address: "9650 Harvest Hills Blvd N #925, Calgary, AB T3K 0B3",
+        latitude: 51.14055454945195, 
+        longitude: -114.06790509812238,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 9/10"
+    },
+
+    {
+        name: "Yum Yum BBQ Chicken 🍗",
+        type: 'chicken',
+        address: "999 36 St NE, Calgary, AB T2A 7X6",
+        latitude: 51.06088140917355,  
+        longitude: -113.98411812091017,
+        rating: "⭐⭐⭐⭐ 4/10"
+    },
+
+    {
+        name: "Seniore's Pizza Restaurant",
+        type: 'pizza',
+        address: "176 Bedford Dr NE #5, Calgary, AB T3K 2M9",
+        latitude:51.12701926705517, 
+        longitude: -114.05589008018688 ,
+        rating: "⭐⭐⭐⭐⭐⭐⭐⭐⭐ 9/10"
+    },
+
+    {
+        name: "Papa John's Pizza",
+        type: 'pizza',
+        address: "18 Arbour Lake Way NW, Calgary, AB T3G 5X4",
+        latitude:51.137960712979876, 
+        longitude: -114.18553428352234, 
+        rating: "⭐⭐⭐⭐⭐ 5/10"
+    },
+
+    {
+        name: "Varsity Pizza & Subs",
+        type: 'pizza',
+        address: "4624 Varsity Dr NW, Calgary, AB T3A 2L9",
+        latitude:51.09355593660302, 
+        longitude: -114.154422064784, 
+        rating: "⭐⭐⭐⭐⭐⭐⭐ 7/10"
+    },
+
+    
+    {
+        name: "Jam's diner",
+        type: 'hamburger',
+        address: "1209 9 Ave SE, Calgary, AB T2G 0S9",
+        latitude:51.04184862050971, 
+        longitude: -114.036216902449, 
+        rating: "⭐⭐⭐⭐⭐⭐⭐ 7/10"
+    },
+
 ]
